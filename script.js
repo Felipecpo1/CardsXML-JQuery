5cfbb4fb47a7e8751e9ba16241a5e6a00c7c7482
Felipecpo1 <felipeppt1@gmail.com>
2023-11-23 00:33:41 -0300

    Cards Uchiha Done

--- script.js
+++ script.js
@@ -5,30 +5,25 @@ $.ajax(url)
     .done(function(xml){
         $(xml).find('uchiha').each(function(){
             $('.swiper-wrapper').append(`<div class = 'swiper-slide'><div class='rosto'><img src='${$(this).find('foto').text()}' class='image-card'></div><div class='oculto'><p class='nome'> ${ $(this).find('nome').text()}</p> <p class='idade'> Idade: ${ $(this).find('idade').text()}</p><p class='olhos'> Olhos: ${ $(this).find('olhos').text()}</p><p class='jutsu'> Principais Jutsus: ${ $(this).find('jutsu').text()}</p><p class='status'> Status: ${ $(this).find('status').text()}</p></div></div`)
-            let click = false;
 
-            $('.swiper').on('click', 'i', function() {
+            let click = true;
+            $('.swiper').on('click', 'img', function() {
               if (!click) {
-                $(this).append(`<i id='nada' class="fa-solid fa-plus">`)
-                $('.oculto').css('display', 'block');
-                $(this).replaceWith(`<i id='tudo' class="fa-solid fa-minus"></i>`);
+                $('p').css('opacity', '0');
                 click = true;
-
-                
               } else {
-                $('.oculto').css('display', 'none');
-                $(this).replaceWith(`<i id='nada' class="fa-solid fa-plus"></i>`);
-                
+                $('p').css('opacity', '1');
                 click = false;
-
+                $('span').css('display', 'none')
               }
             });
-
         })
     })
     .fail(function(){
         alert('Erro de leitura na XML')
     })  
+
+    
 
 
 
